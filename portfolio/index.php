23ad5ba7b8a367996e8f78584d1644615d4f9998
mateobalmes <markmbalmes@gmail.com>
2025-09-19 14:04:40 -0400

    import

---
+++ portfolio/index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="index.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+</body>
+</html>
